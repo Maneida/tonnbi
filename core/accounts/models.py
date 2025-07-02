60f79281a42eb703aa492f8dacb21208fa3af47a
Michael Amuah <mkdamuah@hotmail.com>
2025-07-02 00:49:13 +0000

    partial implementation of core.accounts.models

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class User(AbstractUser):
+    USER_TYPE_CHOICES = [
+        ('buyer', 'Buyer'),
+        ('seller', 'Seller'),
+        ('admin', 'Admin'),
+    ]
+
+emal = models.EmailField(_('email address'), unique=True)
+user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='buyer')
+phone_number = models.CharField(max_length=15, blank=True)
+
